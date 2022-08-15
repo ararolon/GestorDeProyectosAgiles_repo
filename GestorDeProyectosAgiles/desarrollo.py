04c19c6b82f2ac334f25859f393786c710b88ecf
aramy <aramypatricia@gmail.com>
2022-08-15 17:57:36 -0400

    settings de desarrollo y produccion , configuracion de base de datos

---
+++ GestorDeProyectosAgiles/desarrollo.py
@@ -0,0 +1,29 @@
+from decouple import config
+from GestorDeProyectosAgiles.settings import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'GestorDeProyectosAgiles',
+        'USER': 'admin',
+        'PASSWORD': config('PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+
+    }
+}
