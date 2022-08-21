9e178253b65cb39d17e3c9da5d190971283f1eee
aramy <aramypatricia@gmail.com>
2022-08-21 11:19:47 -0400

    configurariones inciciales del sistema

--- SSO/urls.py
+++ SSO/urls.py
@@ -9,7 +9,6 @@ from django.urls import path, include
 from SSO import views
 
 urlpatterns = [
-     path('configuracion_inicial/',views.configuraciones_iniciales,name='confinicial'),
     path('index/', views.index, name='index'), 
     path('',views.configurar_sso,name='configurar_sso'),
     path('login/', views.login, name='login'),
--- SSO/views.py
+++ SSO/views.py
@@ -22,60 +22,51 @@ Vistas de configuraciones iniciales del SSO, login , logout
 def configurar_sso(request):
 
     """
-        Vista para configurar los campos CLIENT_ID y el SECRET_KEY  del Single Sign On.
+       Se realizan las configuraciones iniciales para la identificacion de usuarios.
+       Se crea un grupo administrador y de usuarios si es que todavia no existe en
+       el sistema.
+       Se configuran los campos CLIEND_ID y el SECRET_KEY del Single Sign On.
+       :param request: HttpREquest object
+       :return: HttpRedirect
     """
-   # if not SocialApp.objects.filter(name="sso").exists():  
+   
     if  SocialApp.objects.filter(provider='google').exists():  
-        return redirect('login')
+        return redirect('index')
     
+    if not Group.objects.filter(name='administrador').exists():
+       grupo = Group.objects.create(name='administrador')
+       grupo.save()
+
+   #pregunta por el grupo usuarios
+
+    if not Group.objects.filter(name='usuarios'):
+       grupo = Group.objects.create(name='usuarios')
+       grupo.save()
+
     sa = SocialApp.objects.create(name="sso")
-      
     sa.save()
-    
     form = SocialAppForm(request.POST or None, instance=sa)
        
     if request.method == 'POST':
        if form.is_valid():
             form.save()
             
-            return redirect('login')    
+            return redirect('index')    
         
     contexto = {'form':form}
     return render(request,'SSO/configurar.html',context=contexto)    
     
-    
-
-def configuraciones_iniciales(request):
-
-   """
-    Se realizan las configuraciones iniciales para la identificacion de usuarios.
-    Se crea un grupo administrador y de usuarios si es que todavia no existe en
-    el sistema.
-   :param request: HttpREquest object
-   :return: HttpRedirect
-   """
-   #pregunta por el grupo administrador
-   if not Group.objects.filter(name='administrador').exists():
-       grupo = Group.objects.create(name='administrador')
-       grupo.save()
-
-   #pregunta por el grupo usuarios
-
-   if not Group.objects.filter(name='usuarios'):
-       grupo = Group.objects.create(name='usuarios')
-       grupo.save()
-
-   return redirect('SSO/index')
-
 
 def login(request):
     """
-    Vista que redirecciona al SSO de google
+    Vista que redirecciona al SSO de google, primero verifica que el SSO este configurado
     :param request: HttpRequest object
     :return: HttpRedirect
     """
-    return redirect('/accounts/google/login/?process=login')
+    if  not SocialApp.objects.filter(provider='google').exists():  
+        return redirect('configurar_sso')     
 
+    return redirect('/accounts/google/login/?process=login')
 
 def logout(request):
     """
@@ -86,7 +77,7 @@ def logout(request):
     return redirect('/accounts/logout/')
 
 
-@login_required
+@login_required(login_url='login')
 @decoradores.agregar_usuarios
 def index(request):
     """
@@ -96,7 +87,17 @@ def index(request):
     :return: HttpRedirect
     
     """
-    return render(request, 'SSO/index.html',context=None)
+    #Pregunta si es el administrador del sistema o si es un usuario comun. 
+    #Redirecciona a diferentes interfaces
+
+    #if request.user.groups.filter(name='administrador'):
+    #    return render(request, 'SSO/Home.html', context=None)
+    #elif request.user.groups.filter(name='usuarios'):
+    #    return render(request,'SSO/Home_usuario.html',context=None)
+    #else:
+    #    return redirect('login')
+
+    return render(request,'SSO/index.html',context=None)
     
 
 
