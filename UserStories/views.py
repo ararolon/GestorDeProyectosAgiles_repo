f386413515f6d600b8dc850389d8151fc7b487e8
aramy <aramypatricia@gmail.com>
2022-09-18 03:09:43 -0400

    crear tipo de user story , limitar views

--- UserStories/views.py
+++ UserStories/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .form import EstadosKanbanForm,TiposUSForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required, permission_required
 # Create your views here.
 
 """
  vistas relacionadas a la app UserStories
 """
-
+@login_required(login_url='login')
+@permission_required('permisos._crear_tipos_us',login_url='sinpermiso')
 def crear_estadokanban(request):
   
    """
@@ -35,4 +37,32 @@ def crear_estadokanban(request):
    return render(request, 'UserStories/crear_estado.html',contexto)
 
 
+@login_required(login_url='login')
+@permission_required('permisos._crear_tipos_us',login_url='sinpermiso')
+def crear_TipoUS(request):
 
+    """
+    Formulario para crear un nuevo tipo de US en el sistema
+    
+    Argumentos:
+        request: HttpRequest
+    Retorna:
+        HttpResponse
+    """
+
+    contexto = {'user': request.user}
+    contexto['form'] = TiposUSForm()
+
+    if request.method == 'POST':
+        form = TiposUSForm(request.POST)
+        if form.is_valid():
+            tipoUS = form.save()
+            tipoUS.save()
+            messages.success(request,"El Tipo de US "+tipoUS.nombre+" ha sido creado satisfactoriamente")
+            #return redirect('listar_roles')
+        else:
+            contexto['mensajeError'] = 'El Tipo de US ya existe en el sistema'
+    else:
+        contexto['form'] = TiposUSForm()
+
+    return render(request, 'UserStories/crear_tipoUS.html',contexto)
