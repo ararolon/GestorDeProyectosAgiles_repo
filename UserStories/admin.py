f2a862c304cfba793ecae03091b6063a888b5913
aramy <aramypatricia@gmail.com>
2022-09-22 18:18:00 -0400

    user stories proyecto , product backlog

--- UserStories/admin.py
+++ UserStories/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Estados_Kanban,TipoUSerStory
+from .models import Estados_Kanban,TipoUSerStory,UserStories
 
 # Register your models here.
 
 admin.site.register(TipoUSerStory)
 admin.site.register(Estados_Kanban)
+admin.site.register(UserStories)
