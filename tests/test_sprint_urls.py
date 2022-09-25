4c794a01329757970e3d572f04cac1a0b3e8259d
jennystaple <jenny.staple@fpuna.edu.py>
2022-09-25 19:58:40 -0400

    Actualización Pruebas Unitarias

--- tests/test_sprint_urls.py
+++ tests/test_sprint_urls.py
@@ -1,16 +1,31 @@
 import pytest
 from django.test import TestCase
 from django.urls import reverse, resolve
-from Sprint.views import crearSprint, mostrarSprint
+from Sprint.views import *
+from Proyectos.models import Proyecto
+from Sprint.models import Sprint
+
 """
 test para los urls del Sprint
 """
 class Test_sprint_urls(TestCase):
- 
-   #  def test_crearSprint(self):
-   #     url = reverse('crearSprint', args=[self.proyecto.id])
-   #     self.assertEqual(resolve(url).func, crearSprint, "no se pudo crear sprint")
 
-    def test_mostrarSprint(self):
-       url = reverse('mostrarSprint')
-       self.assertEqual(resolve(url).func, mostrarSprint, "no se pudo crear sprint")
+   def setUp(self):
+      self.proyecto = Proyecto.objects.create(nombre='testProyecto', descripcion='testProyecto')
+      self.sprint = Sprint.objects.create(nombre_sprint='testSprint',fecha_inicio='2022-10-01',fecha_fin='2022-10-15', descripcion='testSprint')
+
+   def test_crearSprint(self):
+      url = reverse('crearSprint', args=[self.proyecto.id])
+      self.assertEqual(resolve(url).func, crearSprint, "no se pudo crear sprint")
+
+   def test_mostrarSprint(self):
+      url = reverse('mostrarSprint')
+      self.assertEqual(resolve(url).func, mostrarSprint, "no se pudo mostrar sprint")
+
+   def test_iniciarSprint(self):
+      url = reverse('iniciarSprint', args=[self.sprint.id])
+      self.assertEqual(resolve(url).func, iniciarSprint, "no se pudo iniciar sprint")
+
+   def test_cancelarSprint(self):
+      url = reverse('cancelarSprint', args=[self.sprint.id])
+      self.assertEqual(resolve(url).func, cancelarSprint, "no se pudo cancelar sprint")
--- tests/test_sprint_views.py
+++ tests/test_sprint_views.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
-from django.urls import reverse,Client
+from django.urls import reverse
 from Sprint.views import *
 from Proyectos.models import Proyecto
+from Sprint.models import Sprint
 
 """
 test para los views.py de Sprint
@@ -9,10 +10,9 @@ test para los views.py de Sprint
  
 class Test_sprint_views(TestCase):
     def setUp(self):
-        self.client = Client()
         self.proyecto = Proyecto.objects.create(nombre='test',descripcion='prueba')
-        self.proyecto.save()
-        
+        self.sprint = Sprint.objects.create(nombre_sprint='testSprint',fecha_inicio='2022-10-01',fecha_fin='2022-10-15', descripcion='testSprint')
+    
     def test_crear_Sprint(self):
         path = reverse('crearSprint',args=[self.proyecto.id])
         response = self.client.get(path)
@@ -24,3 +24,14 @@ class Test_sprint_views(TestCase):
         response = self.client.get(path)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'Sprint/mostrarSprint.html')   
+
+    def test_iniciarSprint(self):
+        print(self.sprint.estado_sprint)
+        path = reverse('iniciarSprint', args=[self.sprint.id])
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, 302)
+
+    def test_cancelarSprint(self):
+        path = reverse('cancelarSprint', args=[self.sprint.id])
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, 302)
