e44d83a821472b0f0711366d019e75114577fa70
jennystaple <jenny.staple@fpuna.edu.py>
2022-10-16 22:10:18 -0300

    Capacidad del Sprint

--- Sprint/models.py
+++ Sprint/models.py
@@ -36,9 +36,10 @@ class Sprint(models.Model):
                     default=estadoSprint.EN_PLANIFICACION) #El estado por defecto al empezar es En Planificacion
     id_proyecto = models.IntegerField(null=True)
     # miembros_sprint = models.ManyToManyField(Usuario, related_name='set_miembros_sprint')
-    capacidad = models.IntegerField(verbose_name='Capacidad en horas',default = 0, blank=True,null=True)
-    capacidad_us = models.IntegerField(default=0,blank=True)
-    
+    capacidad = models.IntegerField(verbose_name='Capacidad en horas', null=True, blank=False)
+    capacidad_us = models.IntegerField(default=0,blank=False)
+    capacidad_equipo = models.IntegerField(default=0,blank=False)
+
     def __str__(self):
         return self.nombre_sprint
 
@@ -48,6 +49,13 @@ class Sprint(models.Model):
         else:
             self.estado_sprint = estadoSprint.CANCELADO
         return True
+    
+    def tiene_miembro(self):
+        """
+        Metodo del modelo de Sprint que retorna un booleano en caso
+        que el sprint no tenga miembros asignados.
+        """
+        return SprintMiembros.objects.filter(sprint=self.id).exists()
 
     def validar(self):
         """
